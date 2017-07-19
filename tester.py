f9795689ed75b2e395241ead5f4ae32a0e406cad
Yamajac <zoeyjaconnolly@gmail.com>
2017-07-19 15:42:52 -0700

    GITHUB PLS
    The GUI confuses me :(

--- tester.py
+++ tester.py
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import glob
 
 print(glob.glob(r"C:\Users\Lauren\Desktop\Media\wallpapers\sorted\**\*.*", recursive=True))
-
-input()
-=======
-from subprocess import run
-run([r"C:\Program Files\Rainmeter\Rainmeter.exe", r"!refresh", "Desktop-Cycler-RM\Monitor2"])
->>>>>>> d11c5742164773b7f8d25c1ffeef19b68612ccb3
-=======
-from subprocess import run
-run([r"C:\Program Files\Rainmeter\Rainmeter.exe", r"!refresh", "Desktop-Cycler-RM\Monitor2"])
->>>>>>> d11c5742164773b7f8d25c1ffeef19b68612ccb3
