0da54a22ae487da80b2ac7673af4f54429072c67
Yamajac <zoeyjaconnolly@gmail.com>
2017-07-19 15:37:25 -0700

    Pls github

--- main.py
+++ main.py
@@ -31,8 +31,6 @@ def changeWallpaper(monitor):
 	skin_path = os.path.expanduser('~\documents\Rainmeter\Skins\Desktop-Cycler-RM\Monitor{0}\path.txt'.format(monitor))
 	with open(skin_path, 'w') as f:
 		f.write(random.choice(getWallpapers()))
-<<<<<<< HEAD
-<<<<<<< HEAD
 	run([r"C:\Program Files\Rainmeter\Rainmeter.exe", r"!Update", "Desktop-Cycler-RM\Monitor{0}".format(monitor)])
 	return
 	
@@ -42,44 +40,10 @@ def getWallpapers():
 		
 def deleteWallpaper(monitor):
 	confirmation = input("Are you sure you want to delete the wallpaper on monitor {0}? (y\\n)".format(monitor))
-=======
-=======
->>>>>>> d11c5742164773b7f8d25c1ffeef19b68612ccb3
 	run([r"C:\Program Files\Rainmeter\Rainmeter.exe", r"!refresh", "Desktop-Cycler-RM\Monitor{0}".format(monitor)])
 	return
-	
-def getWallpapers():
-	return glob.glob(r"C:\Users\Lauren\Desktop\Media\wallpapers\*")
-		
-		
-def deleteWallpaper(monitor):
-	confirmation = input("Are you sure you want to delete the wallpaper? (y\\n)")
-<<<<<<< HEAD
->>>>>>> d11c5742164773b7f8d25c1ffeef19b68612ccb3
-=======
->>>>>>> d11c5742164773b7f8d25c1ffeef19b68612ccb3
-	if confirmation == 'y':
-		wallpaper = getWallpaperFromMonitor(monitor)
-		os.remove(wallpaper)
-		changeWallpaper(monitor)
-	else:
-		return
 
 if __name__ == '__main__':
-<<<<<<< HEAD
-<<<<<<< HEAD
-	if len(sys.argv) > 1:
-		if sys.argv[1] == 'delete':
-			deleteWallpaper(sys.argv[2])
-		else:
-			changeWallpaper(sys.argv[1])
-	else:
-		monitor = str(readMonitorFile())
-		changeWallpaper(monitor)
-	
-		
-=======
-	
 	if len(sys.argv) > 1:
 		if sys.argv[1] == 'delete':
 			deleteWallpaper(sys.argv[2])
@@ -90,17 +54,3 @@ if __name__ == '__main__':
 		changeWallpaper(monitor)
 	
 	
->>>>>>> d11c5742164773b7f8d25c1ffeef19b68612ccb3
-=======
-	
-	if len(sys.argv) > 1:
-		if sys.argv[1] == 'delete':
-			deleteWallpaper(sys.argv[2])
-		else:
-			changeWallpaper(sys.argv[1])
-	else:
-		monitor = str(readMonitorFile())
-		changeWallpaper(monitor)
-	
-	
->>>>>>> d11c5742164773b7f8d25c1ffeef19b68612ccb3
